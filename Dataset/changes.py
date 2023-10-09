fb974a386ba043db3dcc32ab3a393d568eadbb8d
rutvip <116975316+rutvip@users.noreply.github.com>
2023-10-09 17:36:58 -0400

    Create changes.py

---
+++ Dataset/changes.py
@@ -0,0 +1,42 @@
+#changes
+import random
+from datetime import datetime, timedelta
+
+# Define constants
+NUM_RECORDS = 19999  # From CHG-10001 to CHG-19999
+MAX_DAYS_DIFF = 7  # Max number of days between StartDate and EndDate
+SERVER_ID_RANGE = 1000  # Server IDs from SRV-0000001 to SRV-0010000
+DESCRIPTIONS = ["Windows Patching", "Linux patching", "Database upgrade", "Firmware update", "Network reconfiguration"]
+
+# Method to generate a random date within the last two years
+def random_date():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=2*365)
+    random_date = start_date + (end_date - start_date) * random.random()
+    return random_date
+
+# Method to generate a random list of servers
+def random_servers():
+    num_servers = random.randint(1, SERVER_ID_RANGE)
+    servers = random.sample(range(1, SERVER_ID_RANGE + 1), num_servers)
+    return [f"SRV-{server:07}" for server in servers]
+
+# Generate the dataset
+data = []
+
+for _ in range(NUM_RECORDS):
+    start_date = random_date()
+    end_date = start_date + timedelta(days=random.randint(1, MAX_DAYS_DIFF))
+    description = random.choice(DESCRIPTIONS)
+    servers = ", ".join(random_servers())
+
+    data.append({
+        "ID": f"CHG-{10001 + _}",
+        "StartDate": start_date.strftime('%m/%d/%y %H:%M'),
+        "EndDate": end_date.strftime('%m/%d/%y %H:%M'),
+        "Description": description,
+        "AffectedServer": servers
+    })
+
+# Return first 5 rows to verify
+data[:5]
