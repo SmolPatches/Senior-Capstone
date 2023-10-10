859483a01324906810e3a1f3f0365e52a3db1885
rutvip <116975316+rutvip@users.noreply.github.com>
2023-10-10 16:07:51 -0400

    Update server.py
    servers_data.csv added

--- Dataset/server.py
+++ Dataset/server.py
@@ -1,11 +1,15 @@
+import random
+import csv
+
 # Define constants and lists for server dataset
 OS_LIST = ["Linux", "Windows"]
 DATA_CENTERS = [f"DC-{i:01}" for i in range(1, 11)]
 PHYSICAL_SERVERS = [f"SRV-010{str(i).zfill(3)}" for i in range(1, 10000)]
 
-# Generate data for all physical servers
-physical_servers_data = []
+# Generate data for all servers
+all_servers_data = []
 
+# Generate data for all physical servers
 for i in range(1, 10000):  # From SRV-010001 to SRV-019999
     server_name = f"SRV-010{i:03}"
     os_type = "VirtualBox"
@@ -13,7 +17,7 @@ for i in range(1, 10000):  # From SRV-010001 to SRV-019999
     is_virtual = "No"
     parent_server = "N/A"
     
-    physical_servers_data.append({
+    all_servers_data.append({
         "Name": server_name,
         "OS": os_type,
         "DataCenter": data_center,
@@ -21,18 +25,21 @@ for i in range(1, 10000):  # From SRV-010001 to SRV-019999
         "ParentServer": parent_server
     })
 
-# Generate data for all virtual servers
-virtual_servers_data = []
+# Add empty rows (you can specify how many you want)
+empty_rows = 10
+for _ in range(empty_rows):
+    all_servers_data.append({})
 
+# Generate data for all virtual servers
 for i in range(1, 10000):  # From SRV-100001 to SRV-199999
     server_name = f"SRV-100{i:03}"
     os_type = random.choice(OS_LIST)
     parent_server = random.choice(PHYSICAL_SERVERS)
     # Extracting the data center from the associated physical server
-    data_center = next(item for item in physical_servers_data if item["Name"] == parent_server)["DataCenter"]
+    data_center = next(item for item in all_servers_data if item.get("Name") == parent_server).get("DataCenter")
     is_virtual = "Yes"
     
-    virtual_servers_data.append({
+    all_servers_data.append({
         "Name": server_name,
         "OS": os_type,
         "DataCenter": data_center,
@@ -40,7 +47,19 @@ for i in range(1, 10000):  # From SRV-100001 to SRV-199999
         "ParentServer": parent_server
     })
 
+# Specify the CSV file name
+csv_file_name = 'servers_data.csv'
 
+# Write all server data to the CSV file
+with open(csv_file_name, mode='w', newline='') as csv_file:
+    fieldnames = ["Name", "OS", "DataCenter", "IsVirtual", "ParentServer"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    
+    # Write the header row
+    writer.writeheader()
+    
+    # Write the data entries, including empty rows
+    for entry in all_servers_data:
+        writer.writerow(entry)
 
-# Return first 5 rows of each dataset to verify
-physical_servers_data[4:9], virtual_servers_data[4:9]
+print(f"Server data has been written to {csv_file_name}")
