282abec6519badee10171d9b6295937c21dd5bef
rob <mdnlss@outlook.com>
2023-11-22 01:40:26 -0500

    add msgs to build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ then
     echo "venv present"
     alias python3="./venv/bin/python3"
 fi
+echo "Starting Dataset Generation, it may take some time"
 python3 dataset/server.py
 python3 dataset/application.py
 python3 dataset/changes.py
 python3 dataset/incidents.py
 python3 dataset/dataCenter.py
+echo "exporting dataste to CSV_TGT"
 mkdir -p CSV_TGT/
 mv *.csv CSV_TGT
